78939974eb8f69d740d2299f310f074d86dd4140
Jonhnatha Trigueiro <joepreludian@gmail.com>
2014-06-18 00:48:15 -0300

    Adicionado handlers das visoes para se trabalhar no sistema

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from gallery.models import Galeria
 
 
 def gallery(request, gallery_id=None):
-    return render(request, 'base.html')
+
+    galerias = Galeria.objects.all()
+    return render(request, 'pages/gallery_select.html', {'lista': galerias})
+
+
+def gallery_detail(request, gallery_id=None):
+
+    galeria = Galeria.objects.get(id=gallery_id)
+    fotos = galeria.fotos.all()
+
+    return render(request, 'pages/gallery_detail.html', {'galeria': galeria,
+                                                         'fotos': fotos})
