efb18fb0eb009be57c85c681d7adb668bbf09f41
Jonhnatha Trigueiro <joepreludian@gmail.com>
2014-06-18 00:49:55 -0300

    Adicionado suporte de visualizacao dos arquivos da pasta media enquanto servidor de desenvolvimento

--- heyphoto/urls.py
+++ heyphoto/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from heyphoto import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r"^admin/", include(admin.site.urls)),
                        url(r'^', include('gallery.urls')))
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
