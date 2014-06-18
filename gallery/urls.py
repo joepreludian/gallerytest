e62832e41243c8246b9570253faf5291b6c2662b
Jonhnatha Trigueiro <joepreludian@gmail.com>
2014-06-18 00:46:11 -0300

    Adicionado suporte a carregar galeria especifica dentro do django

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gallery.views',
                        url(r'^$', 'gallery'),
-                       url(r'^gallery/', 'gallery'))
+                       url(r'^gallery/(?P<gallery_id>\d+)$', 'gallery_detail'))
